784bcd0cee60294ad499a115e3ca33e5d1fa8c3a
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-08-19 02:15:22 +0900

    make my blog1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+import Header from '@/components/Header';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
+import Footer from '@/components/Footer';
+
+const sans = Open_Sans({ subsets: ['latin'] });
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html >
   )
 }
